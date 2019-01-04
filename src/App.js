776bfdf0c73d6358e7528c0b77e09cb1e1a2374e
uopsdod <uopsdod@gmail.com>
2019-01-05 00:15:19 +0800

    first react js component practice

--- src/App.js
+++ src/App.js
@@ -2,27 +2,124 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class FilterableProductTable extends Component {
+	constructor(props){
+		super(props);
+		this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
+		this.handleIsStockedChange = this.handleIsStockedChange.bind(this);
+		this.state = {words: "", isStocked: true};
+		
+	}
+	
+	handleSearchBarChange(newWords){
+		this.setState({words: newWords});
+	}
+
+	handleIsStockedChange(isStockedOrNot){
+		console.log("FilterableProductTable.handleIsStockedChange called - isStockedOrNot: " + isStockedOrNot);
+		this.setState({isStocked: isStockedOrNot});
+	}	
+	
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+		<SearchBar  words={this.state.words} 
+					isStocked={this.state.isStocked} 
+					handleSearchBarChange={this.handleSearchBarChange}
+					handleIsStockedChange={this.handleIsStockedChange}
+					/>
+		<br/>
+		<ProductTable words={this.state.words} isStocked={this.state.isStocked} />
+		<br/>		
       </div>
     );
   }
 }
 
-export default App;
+class SearchBar extends Component {
+	constructor(props){
+		super(props);		
+		this.handleSearchBarChange = this.handleSearchBarChange.bind(this);
+		this.handleIsStockedChange = this.handleIsStockedChange.bind(this);
+	}
+	
+	handleSearchBarChange(e){
+		this.props.handleSearchBarChange(e.target.value);
+	}
+	
+	handleIsStockedChange(e){
+		this.props.handleIsStockedChange(e.target.checked);
+	}
+		
+	render(){
+		return (
+			<div>
+				<input type="text" value={this.props.words} onChange={this.handleSearchBarChange} />
+				<br/>
+				<input type="checkbox" defaultChecked={this.props.isStocked} onChange={this.handleIsStockedChange} />
+				Only show products in stock 
+			</div>
+		);
+	}
+}
+
+class ProductTable extends Component {
+	render(){
+		
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<th>Price</th>
+					</tr>
+					<ProductCategoryRow name="Sporting Goods"/>
+					<ProductRow name="Football" price="$49.99" stocked={true} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+					<ProductRow name="Baseball" price="$49.99" stocked={true} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+					<ProductRow name="Bastekball" price="$49.99" stocked={false} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+					<ProductCategoryRow name="Electronics"/>
+					<ProductRow name="iPod Touch" price="$49.99" stocked={true} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+					<ProductRow name="iPhone 5" price="$49.99" stocked={false} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+					<ProductRow name="Nexus 7" price="$49.99" stocked={true} words={this.props.words} showOnlyStocked={this.props.isStocked} />
+				</tbody>	
+			</table>
+		)
+	}
+}
+
+class ProductCategoryRow extends Component {
+	render(){
+		return (
+			<tr>
+				<td>{this.props.name}</td>
+			</tr>
+		);
+	}
+}
+
+class ProductRow extends Component {
+
+	render(){
+		if (!this.props.name.includes(this.props.words)){
+			return null;
+		}
+		if (this.props.showOnlyStocked) {
+			if (!this.props.stocked){
+				return null;
+			}
+		}
+		
+		
+		return (
+			<tr>
+				<td>{this.props.name}</td>
+				<td>{this.props.price}</td>
+			</tr>
+		);
+	}
+}
+
+
+
+export default FilterableProductTable;
